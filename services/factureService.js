ed741f7893d9e973726d445803865862abfb58a5
Morojs <moro.hicham@emsi-edu.ma>
2021-06-02 23:34:20 +0200

    Implements "Produit, Livraison, Facture, Localisation" Services

---
+++ services/factureService.js
@@ -0,0 +1,70 @@
+const axios = require("axios").default;
+
+const token = "eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJqYXZhaW51c2UiLCJleHAiOjE2MjI2NzEzOTksImlhdCI6MTYyMjY1MzM5OX0.yzll8R7qCe7cN3gNKYugVmj4ju5MlOA2JI3M2I5gtH9HNUyaSACsg3bzTQuIbkcs6GqKGsgmAhfSKAiS31fGuQ";
+
+const instance = axios.create({
+  timeout: 10000,
+  headers: {
+    Authorization: "Bearer " + token,
+    "Content-Type": "application/json",
+  },
+});
+
+// GET ALL
+module.exports.getFacture = async function () {
+  try {
+    const response = await instance.get(
+      "http://localhost:8081/api/v1/shops/facture"
+    );
+    return response;
+  } catch (error) {
+    console.error(error);
+  }
+};
+//DELETE BY ID
+module.exports.deleteById = async function (id) {
+  try {
+    const response = await instance.delete(
+      "http://localhost:8081/api/v1/shops/facture/" + id
+    );
+    return response;
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+// Add New
+module.exports.addNew = async function (req) {
+  try {
+    const response = await instance.post(
+      "http://localhost:8081/api/v1/shops/facture/",
+      {
+        idFacture: "",
+        dateFacture: req.body.dateFcture,
+        montantFacture: req.body.montantFacture,
+        idCommande: req.body.idCommande,
+        idPayement: req.body.idPayement,
+      }
+    );
+    return response;
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+//UPDATE
+module.exports.updateByID = async function (req) {
+  try {
+    const response = await instance.put(
+      "http://localhost:8081/api/v1/shops/facture/" + req.body.idFacture,
+      {
+        montantFacture: req.body.montantFacture,
+        idCommande: req.body.idCommande,
+        idPayement: req.body.idPayement,
+      }
+    );
+    return response;
+  } catch (error) {
+    console.error(error);
+  }
+};
---
+++ services/produitService.js
@@ -0,0 +1,77 @@
+const axios = require("axios").default;
+
+const token = "eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJqYXZhaW51c2UiLCJleHAiOjE2MjI2NzEzOTksImlhdCI6MTYyMjY1MzM5OX0.yzll8R7qCe7cN3gNKYugVmj4ju5MlOA2JI3M2I5gtH9HNUyaSACsg3bzTQuIbkcs6GqKGsgmAhfSKAiS31fGuQ";
+
+const instance = axios.create({
+  timeout: 10000,
+  headers: {
+    Authorization: "Bearer " + token,
+    "Content-Type": "application/json",
+  },
+});
+
+// GET ALL
+module.exports.getProduit = async function () {
+  try {
+    const response = await instance.get(
+      "http://localhost:8081/api/v1/shops/produit"
+    );
+    return response;
+  } catch (error) {
+    console.error(error);
+  }
+};
+//DELETE BY ID
+module.exports.deleteById = async function (id) {
+  try {
+    const response = await instance.delete(
+      "http://localhost:8081/api/v1/shops/produit/" + id
+    );
+    return response;
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+// Add New
+module.exports.addNew = async function (req) {
+  try {
+    const response = await instance.post(
+      "http://localhost:8081/api/v1/shops/produit/",
+      {
+        idProduit: "",
+        nom: req.body.nom,
+        reference: req.body.reference,
+        description: req.body.description,
+        prix: req.body.prix,
+        qteStock: req.body.qteStock,
+        idBoutique: req.body.idBoutique,
+        id: req.body.idCategorie,
+      }
+    );
+    return response;
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+//UPDATE
+module.exports.updateByID = async function (req) {
+  try {
+    const response = await instance.put(
+      "http://localhost:8081/api/v1/shops/produit/" + req.body.idProduit,
+      {
+        nom: req.body.nom,
+        reference: req.body.reference,
+        description: req.body.description,
+        prix: req.body.prix,
+        qteStock: req.body.qteStock,
+        idBoutique: req.body.idBoutique,
+        id: req.body.idCategorie,
+      }
+    );
+    return response;
+  } catch (error) {
+    console.error(error);
+  }
+};
